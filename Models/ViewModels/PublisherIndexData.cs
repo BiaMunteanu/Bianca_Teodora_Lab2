2fc4b39868b401538357ae32dca58ea12d7b2f1f
BiaMunteanu <mntbianca422@gmail.com>
2022-11-26 18:02:31 +0200

    lab4 + sarcina

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Bianca_Teodora_Lab2.Models;
+
+
+namespace Bianca_Teodora_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -22,8 +22,14 @@ namespace Bianca_Teodora_Lab2.Pages.Books
 
         public IActionResult OnGet()
         {
+            var authorList = _context.Author.Select(x => new
+            {
+                x.ID,
+                FullName = x.FirstName + " " + x.LastName
+            });
+
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
-            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "FirstName");
+            ViewData["AuthorID"] = new SelectList(authorList, "ID", "FullName");
             var book = new Book();
             book.BookCategories = new List<BookCategory>();
             PopulateAssignedCategoryData(_context, book);
